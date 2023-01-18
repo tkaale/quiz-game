5fb377568ca3bd5598f02842b3f68c4bef0edd3c
Aleksandra <tkaczyk.aleksandra@outlook.com>
2023-01-18 13:56:34 +0100

    create question bank from question data

--- main.py
+++ main.py
@@ -6,3 +6,17 @@ class Question:
         self.text = q_text
         self.answer = q_answer
 
+
+def create_question_bank(question_data):
+    question_bank = []
+    for question in question_data:
+        question_text = question['text']
+        question_answer = question['answer']
+        question_bank.append(Question(question_text, question_answer))
+    return question_bank
+
+
+print(create_question_bank(data.question_data))
+
+
+#{'text': "A slug's blood is green.", 'answer': 'True'}
